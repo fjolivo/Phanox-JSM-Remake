6cb56528e7c74fbb1c2077dba6c27560f6dc7770
fjolivo <fjolivo@gmail.com>
2022-11-22 22:02:58 -0400

    Code update

---
+++ ecommerce/lib/client.js
@@ -0,0 +1,10 @@
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+const client = sanityClient({
+    projectID: 'vfxfwnaw',
+    dataset: 'production',
+    apiVesion: '2022-03-10',
+    useCdn: true,
+    token:
+})
