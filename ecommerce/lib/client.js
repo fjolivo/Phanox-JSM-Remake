4f11a5acfc2e7d3049d0e58ef18a5892b96951b4
fjolivo <fjolivo@gmail.com>
2022-11-22 22:16:11 -0400

    Sanity API token configuration

--- ecommerce/lib/client.js
+++ ecommerce/lib/client.js
@@ -1,10 +1,14 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-const client = sanityClient({
-    projectID: 'vfxfwnaw',
+export const client = sanityClient({
+    projectID: 'g246chtn',
     dataset: 'production',
     apiVesion: '2022-03-10',
     useCdn: true,
-    token:
+    token: process.env.NEXT_PUBLIC_SANITY_TOKEN
 })
+
+const builder = imageUrlBuilder(client)
+
+export const urlFor = (source) => builder.image(source);
