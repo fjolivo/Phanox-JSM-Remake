1e98e6f9d5d8415647da392e5c32d26e5b4c32a6
fjolivo <fjolivo@gmail.com>
2022-11-25 23:57:08 -0400

    Fine looking without footer

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import { Layout } from '../components';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Component {...pageProps} />
+  )
 }
 
 export default MyApp
