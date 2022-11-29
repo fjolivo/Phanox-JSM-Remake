4856e13b76c728dd373dcde7c994ba29aa908ffb
fjolivo <fjolivo@gmail.com>
2022-11-29 12:23:02 -0400

    Cart image and buttons added and fixed

--- ecommerce/components/Cart.jsx
+++ ecommerce/components/Cart.jsx
@@ -41,7 +41,7 @@ const Cart = () => {
 
         <div className="product-container">
           {cartItems.length >= 1 && cartItems.map((item) => (
-            <div classname="product" key={item._id}>
+            <div className="product" key={item._id}>
               <img src={urlFor(item?.image[0])} className="cart-product-image" />
               <div className="item-desc">
                 <div className="flex top">
@@ -68,6 +68,19 @@ const Cart = () => {
             </div>
           ))}
         </div>
+        {cartItems.length >= 1 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h3>Subtotal:</h3>
+              <h3>${totalPrice}</h3>
+            </div>
+            <div className="btn-container">
+              <button type="button" className="btn" onClick="">
+                Pay with Stripe
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
