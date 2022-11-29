5d9aa019ebac936867369375e15174e1ff4f473d
fjolivo <fjolivo@gmail.com>
2022-11-29 15:11:18 -0400

    Cart item counting problems

--- ecommerce/context/StateContext.js
+++ ecommerce/context/StateContext.js
@@ -10,6 +10,9 @@ export const StateContext = ({ children }) => {
     const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
+    let foundProduct;
+    let index;
+    
     const onAdd = (product, quantity) => {
         const checkProductInCart = cartItems.find((item) => item._id === product._id);
 
@@ -33,7 +36,26 @@ export const StateContext = ({ children }) => {
 
         toast.success(`${qty} ${product.name} added to the cart.`);
     }
+    
+    const toggleCartItemQuantity = (id, value) => {
+        foundProduct = cartItems.find((item) => item._id ===  id)
+        index = cartItems.findIndex((product) => product._id === id);
 
+        if(value === 'inc') {
+            setCartItems([...cartItems, { ...foundProduct, quantity: foundProduct.quantity + 1}]);
+            setTotalPrice((prevTotalPrice) =>  prevTotalPrice + foundProduct.price)
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+
+        } else if (value === 'dec'){
+            if (foundProduct.quantity > 1) {
+            setCartItems([...cartItems, { ...foundProduct, quantity: foundProduct.quantity - 1}]);
+            setTotalPrice((prevTotalPrice) =>  prevTotalPrice - foundProduct.price)
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+            }
+        }
+
+
+    }
 
     const incQty = () => {
         setQty((prevQty) => prevQty + 1);
@@ -59,6 +81,7 @@ export const StateContext = ({ children }) => {
             incQty,
             decQty,
             onAdd,
+            toggleCartItemQuantity
         }}
         >
             { children }
