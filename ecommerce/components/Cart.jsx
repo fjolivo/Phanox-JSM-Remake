99c993cafb8d96e6da810c35d006e8573348d2af
fjolivo <fjolivo@gmail.com>
2022-11-30 17:21:24 -0400

    Payment Success with Stripe

--- ecommerce/components/Cart.jsx
+++ ecommerce/components/Cart.jsx
@@ -12,7 +12,7 @@ const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } = useStateContext();
 
-  const handleCheckout = () => {
+  const handleCheckout = async () => {
     const stripe = await getStripe();
 
     const response = await fetch('/api/stripe', {
