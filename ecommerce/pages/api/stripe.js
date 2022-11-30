a0ad787c6bae78fa1ee229e5c7fb385f427bab60
fjolivo <fjolivo@gmail.com>
2022-11-30 15:19:28 -0400

    Stripe configuration
    Issue appeared with "await" function

--- ecommerce/pages/api/stripe.js
+++ ecommerce/pages/api/stripe.js
@@ -1,14 +1,36 @@
-import Stripe from 'stripe';
-
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
-    if(req.method === 'POST') {
-        try {
-
-        } catch (error) {
-            res.status(500).json({ statusCode: 500, message: error.message })
-            
-        }
+  if (req.method === 'POST') {
+    try {
+        const params = {
+            submit_type: 'pay',
+            mode:'payment',
+            patment_method_types: ['card'],
+            billing_address_collection: 'auto',
+            shipping_options: [
+                { shipping_rate: 'shr_1M9r80AbXvFnnyrLwFi25Etw'},
+                { shipping_rate: 'shr_1M9r9MAbXvFnnyrLZWaIW60e'},
+            ],
+            line_items: [
+              {
+                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                price: '{{PRICE_ID}}',
+                quantity: 1,
+              },
+            ],
+            mode: 'payment',
+            success_url: `${req.headers.origin}/?success=true`,
+            cancel_url: `${req.headers.origin}/?canceled=true`,
+          }
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(paarams);
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
     }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
 }
